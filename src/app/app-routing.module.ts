55e4ed725bf342eb2de070d2d7ed79e8e94aaab9
luisf4 <luismarcelino.dev@gmail.com>
2023-10-17 16:01:50 -0300

    style: added logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './stocks/home/home.component';
 import { ChartsComponent } from './stocks/charts/charts.component';
+import { SearchStocksComponent } from './stocks/search-stocks/search-stocks.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
   {path: "charts", component: ChartsComponent},
+  {path: "search", component: SearchStocksComponent},
 ];
 
 @NgModule({
